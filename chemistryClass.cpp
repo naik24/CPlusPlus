94e1c8e77113b7cab690209e0bb78145329f940f
Prasad Naik <prasadnaik2499@gmail.com>
2024-10-06 16:34:08 -0400

    chemistry class

--- chemistryClass.cpp
+++ chemistryClass.cpp
@@ -28,3 +28,49 @@ double getTotal(const double numbers[], int size){
 
     return total;
 }
+
+double getLowest(const double numbers[], int size){
+
+    double lowest;
+
+    lowest = numbers[0];
+
+    for(int count = 1; count < size; count++){
+        if(numbers[count] < lowest){
+            lowest = numbers[count];
+        }
+    }
+
+    return lowest;
+}
+
+int main(){
+    const int SIZE = 4;
+    double testScores[SIZE], total, lowestScore, average;
+
+    // set up numeric output formatting
+    cout << fixed << showpoint << setprecision(1);
+
+    // get the test scores from user
+    getTestScores(testScores, SIZE);
+
+    // get the total of the test score
+    total = getTotal(testScores, SIZE);
+
+    // get the lowest test score
+    lowestScore = getLowest(testScores, SIZE);
+
+    // subtract the lowest score from the total
+    total -= lowestScore;
+
+    // calculate the average. Divide by 3 because
+    // the lowest test score was dropped
+    average = total / (SIZE - 1);
+
+    // display the average
+    cout << "The average with the lowest score "
+         << "dropped is " << average << ".\n";
+
+    return 0;
+
+}
